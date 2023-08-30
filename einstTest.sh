7e2cb4d8e45f3715edf18a3d01d144ef5d5f15a2
Cyclopropinon <119750500+Cyclopropinon@users.noreply.github.com>
2023-08-30 22:27:08 +0200

    Create einstTest.sh

---
+++ einstTest.sh
@@ -0,0 +1,8 @@
+whoami
+einst list
+einst 0 https://raw.githubusercontent.com/Cyclopropinon/randomTests/main/demo.sh
+einst list
+einst 7 nerzuigcerzmcgibuer7777777777agui
+einst 8 bzuo888888888888segbou
+einst 5 nuseripvnuip5555555grd
+whoami
