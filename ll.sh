9f79dca9f4fb47176368e2fe93631449b0f97320
Cyclopropinon <119750500+Cyclopropinon@users.noreply.github.com>
2023-12-09 17:36:18 +0100

    Update ll.sh

--- ll.sh
+++ ll.sh
@@ -1,4 +1,12 @@
+einst list
+who
+wifi
+alias
+lsusb
+lscpu
+apt list --installed
+neofetch
+history
 ls -lAR ~
-#einst list
 einst flush
 #einst 0 https://raw.githubusercontent.com/Cyclopropinon/randomTests/main/demo.sh
